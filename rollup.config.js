e8dcc6c0aea3c0c2815bae68a262c76078b317b1
Naoto Kato <naoto.kato@plaid.co.jp>
2019-06-02 07:25:54 +0900

    [package] add package.json, rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,103 @@
+// rollup.config.js
+
+import fs from 'fs'
+
+// import babel        from 'rollup-plugin-babel'
+import importAlias  from 'rollup-plugin-import-alias'
+import nodeResolve  from 'rollup-plugin-node-resolve'
+import commonjs     from 'rollup-plugin-commonjs'
+import nodeBuiltins from 'rollup-plugin-node-builtins'
+import nodeGlobals  from 'rollup-plugin-node-globals'
+
+// import {terser}     from 'rollup-plugin-terser'
+// import {sizeSnapshot} from "rollup-plugin-size-snapshot"
+import analyze from 'rollup-plugin-analyzer'
+
+import vue          from 'rollup-plugin-vue'
+import postcssFontAwesome from 'postcss-font-awesome'
+
+import webWorkerLoader from 'rollup-plugin-web-worker-loader'
+
+const base = {
+
+  plugins: [
+    importAlias({
+      Paths: {
+        "opencv_js": __dirname+"/src/libs/opencv_js.js",
+        "opencv": __dirname+"/src/libs/opencv.js",
+        "libs": "./src/libs"
+      },
+    }),
+
+    webWorkerLoader({
+      inline: false,
+      loadPath: '/assets/'
+    }),
+
+    // CommonJSモジュールをES6に変換
+    commonjs({
+      // exclude: ['**/*.worker.js'],
+      extensions: [ '.js' ],
+    }),
+
+    nodeGlobals(),
+
+    nodeBuiltins(),
+
+    // npmモジュールを`node_modules`から読み込む
+    nodeResolve({ jsnext: true }),
+
+    // .vueのrequire
+    vue({
+      css: true, // dynamically inject
+      postcss: [
+        postcssFontAwesome({
+          replacement: false
+        })
+      ],
+    }),
+
+    // babel({
+    //   exclude: 'node_modules/**',
+    //   runtimeHelpers: true,
+    //   babelrc: false,
+    //   presets: [
+    //     [
+    //       '@babel/env',
+    //       {
+    //         modules: false,
+    //         "targets": {
+    //           "browsers": [
+    //             "> 1%",
+    //             "last 2 versions",
+    //             "not ie <= 12"
+    //           ]
+    //         }
+    //       }
+    //     ]
+    //   ],
+    //   "plugins": [
+    //     "transform-es2015-destructuring",
+    //   ]
+    // }),
+
+    analyze({
+      writeTo: (analysisString) => fs.writeFileSync('analysis.txt', analysisString)
+    }),
+
+    // sizeSnapshot(),
+
+    // terser(),
+  ],
+}
+
+
+export default Object.assign({
+    input: `./src/index.js`,
+    output: {
+      file: `./public/assets/index.js`,
+      format: 'es',
+      sourcemap: false,
+    }
+  }, base)
+
